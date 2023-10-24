aa5b3682bd8f22074eadda533c1aa11b1acf64b3
tkachenya41 <tkachenya41@gmail.com>
2023-10-24 10:29:29 +0300

    feat: add 4 routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+app.get("/", () => "Method get created!");
+
+app.post("/post", () => "Method post created!");
+
+app.put("/update", () => "Method put created!");
+
+app.delete("/delete", () => "Method delete created!");
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
