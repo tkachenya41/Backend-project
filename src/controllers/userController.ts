4afe330628e8d0fc554f7f8551a7c5594dd77716
tkachenya41 <tkachenya41@gmail.com>
2023-10-27 23:09:13 +0300

    refactor: add prisma model

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../model/prisma";
 
 export const user = {
   getAllUsers: () => prisma.user.findMany(),
