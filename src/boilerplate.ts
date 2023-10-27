08ed3378271cef255d081b58a19cb60efb48b4e8
tkachenya41 <tkachenya41@gmail.com>
2023-10-27 12:40:29 +0300

    fix: remove needless in User Model

--- src/boilerplate.ts
+++
@@ -1,6 +0,0 @@
-export const boilerplate = {
-  get: () => "Method get created",
-  post: () => "Method post created",
-  put: () => "Method update created",
-  delete: () => "Method delete created",
-};
