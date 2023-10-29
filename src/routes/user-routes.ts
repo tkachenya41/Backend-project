a0220249fd572311499ca7705c9be002a12ccaa8
tkachenya41 <tkachenya41@gmail.com>
2023-10-30 01:16:47 +0300

    refactor: add body schema

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,6 +1,15 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { user } from "../controllers/userController";
-import { Body, Set } from "../utils/utils";
+import { Set } from "../utils/utils";
+
+const userBodyCheck = {
+  body: t.Object({
+    id: t.Number(),
+    name: t.String(),
+    email: t.String(),
+    password: t.String(),
+  }),
+};
 
 export const userRoutes = (app: Elysia) =>
   app.group("/users", (group) =>
@@ -9,7 +18,11 @@ export const userRoutes = (app: Elysia) =>
         user.getById(Number(id), set as Set)
       )
       .get("/", user.getAll)
-      .post("/", ({ body, set }) => user.post(body as Body, set as Set))
-      .put("/", ({ body, set }) => user.put(body as Body, set as Set))
-      .delete("/", ({ body, set }) => user.delete(body as Body, set as Set))
+      .post("/", ({ body, set }) => user.post(body, set as Set), userBodyCheck)
+      .put("/", ({ body, set }) => user.put(body, set as Set), userBodyCheck)
+      .delete(
+        "/",
+        ({ body, set }) => user.delete(body, set as Set),
+        userBodyCheck
+      )
   );
