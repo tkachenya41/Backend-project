62bb444746a40c88fb6017d6d3b2c3947135f0a6
tkachenya41 <tkachenya41@gmail.com>
2023-11-08 22:46:37 +0300

    fix: resolve middleware problem

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -2,11 +2,10 @@ import { DBError } from '@/utils/custom-error';
 import { errorCode } from '@/utils/utils';
 import { validator } from 'hono/validator';
 
-export const validateUserId = async() => {
-  validator('param', (value) => {
+export const validateUserId = validator('param', (value) => {
+    console.log(value,value.id,+value.id)
     if (isNaN(+value.id)) {
       throw new DBError('UserId should be a number.', errorCode.INVALID);
     }
-    return value;
   })
-};
+
